7d2af2a63d10725d5028b589d13b37aa032f07d3
Bunch0fAtoms <Bunch0fAtoms>
2023-07-27 20:23:12 +0000

    dynamically create cosmosDB names and endpoints
    added instructions on creating cosmosDB assets, secrets, and dynamically creating containers and endpoints

--- 04-OnlineFeatureStore.py
+++ 04-OnlineFeatureStore.py
@@ -46,27 +46,35 @@ print(schema_name)
 # MAGIC 1. Go to Azure portal at https://portal.azure.com/
 # MAGIC 2. Search and open "Cosmos DB", then create or select an account.
 # MAGIC 3. Navigate to "keys" the view the URI and credentials.
+# MAGIC 4. You'll paste the Cosmos DB "Read-write Keys" and "Read-only Keys" into the respective Databricks secret.
 # MAGIC
+# MAGIC ### Authenticate Databricks command line (CLI) with your workspace
+# MAGIC
+# MAGIC Use [Databricks CLI](https://learn.microsoft.com/en-us/azure/databricks/dev-tools/cli/databricks-cli) to create secrets. You'll have to [authenticate](https://learn.microsoft.com/en-us/azure/databricks/dev-tools/cli/databricks-cli#--set-up-authentication) between Databricks CLI and Databricks Workspace. 
+# MAGIC 1. Using CLI enter this command to prompt Databricks Workspace authentication 
+# MAGIC ```
+# MAGIC     databricks configure --token
+# MAGIC ```
 # MAGIC ### Provide online store credentials using Databricks secrets
-# MAGIC
 # MAGIC **Note:** For simplicity, the commands below use predefined names for the scope and secrets. To choose your own scope and secret names, follow the process in the Databricks [documentation](https://docs.microsoft.com/azure/databricks/applications/machine-learning/feature-store/online-feature-stores).
 # MAGIC
-# MAGIC 1. Create two secret scopes in Databricks.
+# MAGIC 1. Using CLI, create two secret scopes in Databricks.
 # MAGIC
 # MAGIC     ```
-# MAGIC     databricks secrets create-scope --scope feature-store-example-read
-# MAGIC     databricks secrets create-scope --scope feature-store-example-write
+# MAGIC     databricks secrets create-scope --scope one-env-dynamodb-fs-read
+# MAGIC     databricks secrets create-scope --scope one-env-dynamodb-fs-write
 # MAGIC     ```
 # MAGIC
-# MAGIC 2. Create secrets in the scopes.  
-# MAGIC    **Note:** the keys should follow the format `<prefix>-authorization-key`. For simplicity, these commands use predefined names here. When the commands run, you will be prompted to copy your secrets into an editor.
+# MAGIC 2. Using CLI, add keys to the scopes. Then when prompted, copy/paste the Cosmos DB keys as secrets.
+# MAGIC
+# MAGIC    **Note:** The keys should follow the format `<prefix>-authorization-key`. Give the "prefix" a unique and meaningful name, but be sure the key ends with "-authorization-key". For simplicity, these commands use predefined names here. When the commands run, you will be prompted to copy your Cosmos DB "Read-write Keys" and "Read-only Keys" into an editor.
 # MAGIC
 # MAGIC     ```
-# MAGIC     databricks secrets put --scope feature-store-example-read --key cosmos-authorization-key
-# MAGIC     databricks secrets put --scope feature-store-example-write --key cosmos-authorization-key
+# MAGIC     databricks secrets put --scope one-env-dynamodb-fs-read --key solutionaccelerator-authorization-key
+# MAGIC     databricks secrets put --scope one-env-dynamodb-fs-write --key solutionaccelerator-authorization-key
 # MAGIC     ```
 # MAGIC     
-# MAGIC Now the credentials are stored with Databricks Secrets. You will use them below to create the online store.
+# MAGIC Now the credentials are stored with Databricks Secrets. You will use them below to create the online feature store.
 
 # COMMAND ----------
 
@@ -75,8 +83,47 @@ print(schema_name)
 
 # COMMAND ----------
 
-# DBTITLE 1,Define Online Feature Store Spec, Publish First Table
+# DBTITLE 1,Define Online Feature Store Spec. Publish First FS Table (drug_features)
+# Specify the online store.
+# Note: These commands use the predefined secret prefix. If you used a different secret scope or prefix, edit these commands before running them.
+#       Make sure you have a database created with same name as specified below.
+#       Do not manually create the database or container in Cosmos DB. The publish_table() command creates it for you.
+
+online_store_spec = AzureCosmosDBSpec(
+  account_uri=account_uri,
+  read_secret_prefix="one-env-dynamodb-fs-read/solutionaccelerator",
+  write_secret_prefix="one-env-dynamodb-fs-write/solutionaccelerator",
+  database_name=f"{user_name}_solution_accelerator_patient_risk_scoring_feature_store",
+  container_name="drug_features"
+)
+
+# Push the feature table to online store.
+fs.publish_table(f'{feature_schema}.drug_features', online_store_spec)
+
+# COMMAND ----------
+
+# DBTITLE 1,Publish Second FS Table (condition_history_features)
+# Specify the online store.
+# Note: These commands use the predefined secret prefix. If you used a different secret scope or prefix, edit these commands before running them.
+#       Make sure you have a database created with same name as specified below.
+#       Do not manually create the database or container in Cosmos DB. The publish_table() command creates it for you.
+
+online_store_spec = AzureCosmosDBSpec(
+  account_uri=account_uri,
+  read_secret_prefix="one-env-dynamodb-fs-read/solutionaccelerator",
+  write_secret_prefix="one-env-dynamodb-fs-write/solutionaccelerator",
+  database_name=f"{user_name}_solution_accelerator_patient_risk_scoring_feature_store",
+  container_name=f"condition_history_features"
+)
+
+# Push the feature table to online store.
+fs.publish_table(f'{feature_schema}.condition_history_features', online_store_spec)
+
+# COMMAND ----------
+
+# DBTITLE 1,Publish Third Table (subject_demographics_features)
 from databricks.feature_store.online_store_spec import AzureCosmosDBSpec
+#Fill in the account_uri of your Azure Cosmos DB account
 account_uri = "https://field-demo.documents.azure.com:443/"
 
 # Specify the online store.
@@ -86,10 +133,10 @@ account_uri = "https://field-demo.documents.azure.com:443/"
 
 online_store_spec = AzureCosmosDBSpec(
   account_uri=account_uri,
-  write_secret_prefix="one-env-dynamodb-fs-write/solutionaccelerator",
   read_secret_prefix="one-env-dynamodb-fs-read/solutionaccelerator",
-  database_name="solution_accelerator_patient_risk_scoring_feature_store",
-  container_name="subject_demographics_features_v3"
+  write_secret_prefix="one-env-dynamodb-fs-write/solutionaccelerator",
+  database_name=f"{user_name}_solution_accelerator_patient_risk_scoring_feature_store",
+  container_name="subject_demographics_features"
 )
 
 # Push the feature table to online store.
@@ -97,50 +144,17 @@ fs.publish_table(f'{feature_schema}.subject_demographics_features', online_store
 
 # COMMAND ----------
 
-# DBTITLE 1,Publish Second FS Table
-# Specify the online store.
-# Note: These commands use the predefined secret prefix. If you used a different secret scope or prefix, edit these commands before running them.
-#       Make sure you have a database created with same name as specified below.
-#       Do not manually create the database or container in Cosmos DB. The publish_table() command creates it for you.
-
-online_store_spec = AzureCosmosDBSpec(
-  account_uri=account_uri,
-  write_secret_prefix="one-env-dynamodb-fs-write/solutionaccelerator",
-  read_secret_prefix="one-env-dynamodb-fs-read/solutionaccelerator",
-  database_name="solution_accelerator_patient_risk_scoring_feature_store",
-  container_name="drug_features_v3"
-)
-
-# Push the feature table to online store.
-fs.publish_table(f'{feature_schema}.drug_features', online_store_spec)
+# MAGIC %md
+# MAGIC # Create Serverless Endpoint for Model Scoring
 
 # COMMAND ----------
 
-# DBTITLE 1,Publish Third Table
-# Specify the online store.
-# Note: These commands use the predefined secret prefix. If you used a different secret scope or prefix, edit these commands before running them.
-#       Make sure you have a database created with same name as specified below.
-#       Do not manually create the database or container in Cosmos DB. The publish_table() command creates it for you.
-
-online_store_spec = AzureCosmosDBSpec(
-  account_uri=account_uri,
-  write_secret_prefix="one-env-dynamodb-fs-write/solutionaccelerator",
-  read_secret_prefix="one-env-dynamodb-fs-read/solutionaccelerator",
-  database_name="solution_accelerator_patient_risk_scoring_feature_store",
-  container_name="condition_history_features_v3"
-)
-
-# Push the feature table to online store.
-fs.publish_table(f'{feature_schema}.condition_history_features', online_store_spec)
-
-# COMMAND ----------
-
-# DBTITLE 1,Load your model name
+# DBTITLE 1,Load your model name from 03-AutoML
 model_name = "omop_patientrisk_model"
 
 # COMMAND ----------
 
-# DBTITLE 1,No longer needed. its in the backend folder
+# DBTITLE 1,No longer needed. its in the backend folder. 00-init-modelserving 
 import time
 import requests 
 
@@ -250,31 +264,32 @@ class EndpointApiClient:
 
 # COMMAND ----------
 
-# DBTITLE 1, Set up & start a Serverless model serving endpoint using the API: We will use the API to programmatically start the endpoint:
+# DBTITLE 1, Set up & start a serverless model serving endpoint using the API
 client = mlflow.tracking.MlflowClient()
 latest_model = client.get_latest_versions(model_name, stages=["Production"])[0]
 
-#See the 00-init-expert notebook for the endpoint API details
+#See the 00-init-modelserving for the endpoint API details
 serving_client = EndpointApiClient()
 
-#Start the enpoint using the REST API (you can do it using the UI directly)
-serving_client.create_endpoint_if_not_exists("patientrisk_feature_store_endpoint_v3", model_name=model_name, model_version = latest_model.version, workload_size="Small", scale_to_zero_enabled=True, wait_start = True)
+#Start the endpoint using the REST API (you can do it using the UI directly)
+serving_client.create_endpoint_if_not_exists(f"{user_name}_patientrisk_feature_store_endpoint", model_name=model_name, model_version = latest_model.version, workload_size="Small", scale_to_zero_enabled=True, wait_start = True)
 
 # COMMAND ----------
 
 # Fill in the Databricks access token value.
 # Note: You can generate a new Databricks access token by going to left sidebar "Settings" > "User Settings" > "Access Tokens", or using databricks-cli.
-
-DATABRICKS_TOKEN = "dapid768198092de9b82bd55a50523b696d5"
+DATABRICKS_TOKEN = "FillinWithYourToken"
 
 # COMMAND ----------
 
 # DBTITLE 1,Functions to Score using Endpoint
+workspacename = spark.conf.get("spark.databricks.workspaceUrl")
+
 def create_tf_serving_json(data):
     return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
 
 def score_model(dataset):
-    url = 'https://e2-demo-field-eng.cloud.databricks.com/serving-endpoints/patientrisk_feature_store_endpoint_v3/invocations'
+    url = f"https://{workspacename}/serving-endpoints/{user_name}_patientrisk_feature_store_endpoint/invocations
     headers = {'Authorization': f'Bearer {DATABRICKS_TOKEN}', 'Content-Type': 'application/json'}
     ds_dict = {'dataframe_split': dataset.to_dict(orient='split')} if isinstance(dataset, pd.DataFrame) else create_tf_serving_json(dataset)
     data_json = json.dumps(ds_dict, allow_nan=True)
@@ -286,9 +301,16 @@ def score_model(dataset):
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## Score patient records!
+
+# COMMAND ----------
+
+# DBTITLE 1,Simulate receiving patient records for scoring
 newrecords = pd.DataFrame([68544,1460,5155,29660,52882,23215,32188,45457,86968], columns=["subject_id"])
 display(newrecords)
 
 # COMMAND ----------
 
+# DBTITLE 1,Score patient records using Endpoint
 print(score_model(newrecords))
